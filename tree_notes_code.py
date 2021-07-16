42c8d4922f7af38ea6c40ba80cc96ccc4f6186be
lbase <lbase@finormile.com>
2021-07-15 22:55:38 -0400

    open file for reading

--- tree_notes_code.py
+++ tree_notes_code.py
@@ -1,4 +1,5 @@
 import sys, os
+from icecream import ic
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import QDir, QFile
 from PyQt5.QtGui import QStandardItem, QStandardItemModel
@@ -15,7 +16,7 @@ class Main(QtWidgets.QMainWindow, Ui_TreeNotesWin):
         self.setuptreeview()
 
     def setuptreeview(self):
-        self.dir = QDir("/home/rfile/revclips")
+        self.dir = QDir("/home/rfile/revclips/")
         self.dir.setNameFilters(["*.txt"])
         self.lst = [os.path.splitext(x)[0] for x in self.dir.entryList()]
         self.treemodel = QStandardItemModel(0, 1)
@@ -23,6 +24,18 @@ class Main(QtWidgets.QMainWindow, Ui_TreeNotesWin):
         for y in (self.lst):
             self.myitem = y
             self.treemodel.appendRow(QStandardItem(self.myitem))
+        self.ui.treeRevNote.doubleClicked.connect(self.getValue)
+
+    def getValue(self, val):
+        #print(val.data())
+        #print(val.row())
+        #print(val.column())
+        self.fdir = self.dir.path() + '/'
+        self.myfname = self.fdir + (val.data() + ".txt")
+        ic(self.myfname)
+        self.fod = open(self.myfname)
+        self.fdata = self.fod.read()
+        self.ui.txtRevNote.setText(self.fdata)
 
 
 if __name__ == "__main__":
