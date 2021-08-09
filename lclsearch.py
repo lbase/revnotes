53a6a7b2b3ac227c90d8fb8fde5a0f36e830d935
lbase <lbase@finormile.com>
2021-08-08 20:08:43 -0400

    fixed indent

--- lclsearch.py
+++ lclsearch.py
@@ -8,35 +8,50 @@ from whoosh import scoring
 from whoosh.index import open_dir
 # test note from bubba
 
+
 class revsearch():
     def __init__(self) -> None:
         super(revsearch, self).__init__()
 
-        def createSearchableData(root):
+        def createSearchableData(self, root="/home/rfile/rev_clips"):
             self.root = root
-            # don't store index in same dir as text files ...
-            self.indexdir = "home/rfile/index/revnotes"
 
             self.schema = Schema(title=TEXT(stored=True),path=ID(stored=True),\
-                    content=TEXT,textdata=TEXT(stored=True))
-
-            if not os.path.exists(self.indexdir):
-                os.mkdir(self.indexdir)
+                    textdata=TEXT(stored=True))
+            ic(self.schema)
+            self.localIndexDir = os.path.join(self.root, "indexdir")
+            ic(self.localIndexDir)
+            if not os.path.exists(self.localIndexDir):
+                os.mkdir(self.localIndexDir)
+                # os.mkdir(localIndexDir + '/')
 
             # Creating a index writer to add document as per schema
-            self.ix = create_in(self.indexdir, self.schema)
+            self.ix = create_in(self.localIndexDir, self.schema)
             self.writer = self.ix.writer()
+            try:
 
-            self.filepaths = [
-                os.path.join(self.root, i) for i in os.listdir(self.root)
-            ]
-            for path in self.filepaths:
-                fp = open(path, 'r')
-                # ic(path)
-                text = fp.read()
-                # ic(text)
-                self.writer.add_document(title=path.split("/")[4], path=path,\
-                content=text,textdata=text)
-                fp.close()
-            self.writer.commit()
+                self.filepaths = self.txtfilelist(root)
+                for path in self.filepaths:
+                    fp = open(path, 'r')
+                    text = fp.read()
+                    self.writer.add_document(title=(os.path.basename(path)),path=path,\
+                    textdata=text)
+
+                    fp.close()
+                self.writer.commit()
+            except:
+                e = sys.exc_info()[0]
+                self.writer.cancel()
+                print("exception " + e)
+
             return self.ix.searcher()
+
+        def txtfilelist(self, root="/home/rfile/rev_clips"):
+            self.root = root
+            self.RevTextFiles = []
+            self.RevFile = []
+            for file in os.listdir(self.root):
+                if file.endswith(".txt"):
+                    self.RevTextFiles.append(os.path.join(root, file))
+                    # RevFile.append(file)
+            return self.RevTextFiles
