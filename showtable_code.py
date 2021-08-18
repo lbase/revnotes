0d3b928de9d9f7f9dbd149f6243f0eadf1d3df72
lbase <lbase@finormile.com>
2021-08-17 20:33:26 -0400

    can strip control characters with regex
    in treenote.ipnb and in revnotes wiki

--- showtable_code.py
+++ showtable_code.py
@@ -29,7 +29,7 @@ class Main(QMainWindow, Ui_TblWin):
         # proxy
         self.proxy_model = QSortFilterProxyModel()
         self.proxy_model.setFilterKeyColumn(
-            3)  # Search all columns is -1 3 should be content
+            -1)  # Search all columns is -1 3 should be content
         self.proxy_model.setSourceModel(self.model)
         # self.proxy_model.dynamicSortFilter()
         self.proxy_model.sort(0, Qt.AscendingOrder)
@@ -46,7 +46,7 @@ class Main(QMainWindow, Ui_TblWin):
         #self.ui.tb1.resizeRowsToContents()
         self.ui.tb1.setColumnHidden(0, 1)
         self.ui.tb1.setColumnHidden(1, 1)
-        self.ui.tb1.setColumnHidden(2, 1)
+        # self.ui.tb1.setColumnHidden(2, 1)
         # self.ui.tb1.setColumnWidth(3, 300)
         #self.ui.tb1.setRowHeight(100)
         # self.ui.tb1.setSelectionBehavior(0)
